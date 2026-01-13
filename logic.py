34a954875460549b3ced16a35c2c8fce8b03b057
Jeffrin James <105354252+JeffrinJamesCODY@users.noreply.github.com>
2026-01-13 09:38:42 +0000

    Create logic.py

---
+++ logic.py
@@ -0,0 +1,12 @@
+import random
+import time
+import pygame
+
+Symbols: ["Mandarin Orange", "Aidan Fruit", "Sakura Cherry", "Jack Fruit", "Cocoa Brownie", "Macha","Jinx Fruit"]
+
+def roll.reels()
+   random[
+       random.choice(Symbols)
+       random.choice(Symbols)
+       random.choice(Symbols)
+   ]
